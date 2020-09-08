dec733f073ed2c8a06962b98100e833534b1f541
Cameron <camtheanaconda@gmail.com>
2020-09-07 19:42:43 -0700

    "Goodbye" message added by partner

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,5 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args)
 	{
 		System.out.println("Hello World");
+		//Partner added line below.
+		System.out.println("Goodbye! Goodbye! ");
 	}
 }
